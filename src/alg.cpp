d1e2c0177218775d203fa83694ef8a97bd95942e
Anton <150729033+biorge@users.noreply.github.com>
2024-03-09 11:42:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -24,7 +24,7 @@ double calcItem(double x, uint16_t n) {
   for (uint16_t i = 0; i < n; i++) {
     res1 *= x;
   }
-  uint64_t res2 = 1;
+  double res2 = 1;
   for (int i = n; i >= 1; i--) {
     res2 *= i;
   }
@@ -38,7 +38,7 @@ double expn(double x, uint16_t count) {
     for (uint16_t j = 0; j < i; j++) {
       res1 *= x;
     }
-    uint64_t res2 = 1;
+    double res2 = 1;
     for (int k = 1; k <= i; k++) {
       res2 *= k;
     }
@@ -49,7 +49,7 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
   double res = x;
-  int16_t znak = -1;
+  double znak = -1;
   for (uint16_t i = 3; i <= count; i += 2) {
     res += znak * (pown(x, i) / fact(i));
     znak *= -1;
@@ -59,7 +59,7 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
   double res = 1;
-  int16_t znak = -1;
+  double znak = -1;
   for (uint16_t i = 2; i <= count; i += 2) {
     res += znak * (pown(x, i) / fact(i));
     znak *= -1;
