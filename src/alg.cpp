55fa0503b53a56ab581c62e55b61e0a86035e30d
Anton <150729033+biorge@users.noreply.github.com>
2024-03-09 11:45:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,8 @@
 #include "alg.h"
 
 
-double pown(double value, uint16_t n) {
-  double res = 1.0;
+long double pown(long double value, uint16_t n) {
+  long double res = 1.0;
   for (uint16_t i = 0; i < n; i++) {
     res *= value;
   }
@@ -19,26 +19,26 @@ uint64_t fact(uint16_t n) {
   return res;
 }
 
-double calcItem(double x, uint16_t n) {
-  double res1 = 1.0;
+long double calcItem(long double x, uint16_t n) {
+  long double res1 = 1.0;
   for (uint16_t i = 0; i < n; i++) {
     res1 *= x;
   }
-  double res2 = 1;
+  long double res2 = 1;
   for (int i = n; i >= 1; i--) {
     res2 *= i;
   }
   return res1 / res2;
 }
 
-double expn(double x, uint16_t count) {
-  double res = 1.0;
+long double expn(long double x, uint16_t count) {
+  long double res = 1.0;
   for (uint16_t i = 1; i <= count; i++) {
-    double res1 = 1.0;
+    long double res1 = 1.0;
     for (uint16_t j = 0; j < i; j++) {
       res1 *= x;
     }
-    double res2 = 1;
+    long double res2 = 1;
     for (int k = 1; k <= i; k++) {
       res2 *= k;
     }
@@ -47,9 +47,9 @@ double expn(double x, uint16_t count) {
   return res;
 }
 
-double sinn(double x, uint16_t count) {
-  double res = x;
-  double znak = -1;
+long double sinn(long double x, uint16_t count) {
+  long double res = x;
+  long double znak = -1;
   for (uint16_t i = 3; i <= count; i += 2) {
     res += znak * (pown(x, i) / fact(i));
     znak *= -1;
@@ -57,9 +57,9 @@ double sinn(double x, uint16_t count) {
   return res;
 }
 
-double cosn(double x, uint16_t count) {
-  double res = 1;
-  double znak = -1;
+long double cosn(long double x, uint16_t count) {
+  long double res = 1;
+  long double znak = -1;
   for (uint16_t i = 2; i <= count; i += 2) {
     res += znak * (pown(x, i) / fact(i));
     znak *= -1;
